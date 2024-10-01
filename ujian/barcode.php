7499b74dca9f457d1d3e61adb69857abbf7b4181
Bagas-Pradana <bagaspradana599@gmail.com>
2024-10-01 08:29:14 +0700

    Add files via upload

---
+++ ujian/barcode.php
@@ -0,0 +1,19 @@
+<?php
+
+include 'barcode1.php';
+
+if(isset($_POST['generateBarcode'])) {
+$barcodeText = trim($_POST['barcodeText']);
+$barcodeType=$_POST['barcodeType'];
+$barcodeDisplay=$_POST['barcodeDisplay'];
+$barcodeSize=$_POST['barcodeSize'];
+$printText=$_POST['printText'];
+if($barcodeText != '') {
+echo '<h4>Barcode:</h4>';
+echo '<img class="barcode" alt="'.$barcodeText.'" src="barcode.php?text='.$barcodeText.'&codetype='.$barcodeType.'&orientation='.$barcodeDisplay.
+'&size='.$barcodeSize.'&print='.$printText.'"/>';
+} else {
+echo '<div class="alert alert-danger">Enter product name or number to generate barcode!</div>';
+}
+}
+?>
